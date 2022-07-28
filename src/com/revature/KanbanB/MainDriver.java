a6b4978bb488f64bc37e3711f6393d249ce1ba77
Handerson <coqhandersonth@gmail.com>
2022-07-28 04:21:51 -0400

    initial commit

---
+++ src/com/revature/KanbanB/MainDriver.java
@@ -0,0 +1,12 @@
+package com.revature.KanbanB;
+
+/* KanbanB is a kanban board for professional developers. Its purpose is to allow members
+to join the application, be assigned part of a team and share their kanban cards.
+*/
+
+public class MainDriver {
+    public static void main(String[] args) {
+        System.out.println("Test: Initial Commit");
+    }
+
+}
